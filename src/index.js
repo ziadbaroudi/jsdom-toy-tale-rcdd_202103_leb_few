9d5551606b0b4b9f77d755b07e18ca225b85c15b
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-05-13 01:31:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let addToy = false;
-
+let toydiv = document.getElementById("toy-collection")
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -12,4 +12,38 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  
+  function fetchToys(){
+    return fetch("http://localhost:3000/toys")
+    .then(res => res.json());
+  }
+  
+  function createCard(){
+    fetchToys().then(toys=> {toys.forEach(toy =>{
+      let h = document.createElement("h2");
+      h.innerText = toy.name;
+      
+      let img = document.createElement("img");
+      img.src = toy.image; 
+      img.className = "toy-avatar";
+      
+      let p = document.createElement("p");
+      p.innerText = `${toy.likes} likes`;
+      
+      let btn = document.createElement("button");
+      btn.className = "like-btn";
+      btn.innerText = "like";
+      // btn.addEventListener('click', ()=>{
+        
+      // })
+      
+      let div = document.createElement('div');
+      div.className = "card";
+      div.appendChild(h, img, p, btn);
+      toydiv.appendChild(div);
+      
+    })});
+   
+  }
+  createCard();
 });
